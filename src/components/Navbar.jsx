5c84614b62eefde7215fe8b33047e135b418de4b
Leendk <leen.dk12@gmail.com>
2025-04-16 19:37:10 +0300

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,6 @@
-
 import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
-
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -10,22 +8,30 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="logo">BOXCARS</div>
-      <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
-        &#9776;
-      </button>
-      <ul className={`menu ${menuOpen ? "show" : ""}`}>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Listings</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Pages</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-      <div className="nav-actions">
-        <button className="sign-in">
-          <FontAwesomeIcon icon={faUser} /> Sign In
+
+      <div className="navbar-content">
+        <ul className={`menu ${menuOpen ? "show" : ""}`}>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Listings</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Pages</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+
+        <div className="nav-actions">
+          <button className="sign-in">
+            <FontAwesomeIcon icon={faUser} /> Sign In
+          </button>
+          <button className="submit-btn">Submit Listing</button>
+        </div>
+
+        <button
+          className="menu-toggle"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          &#9776;
         </button>
-        <button className="submit-btn">Submit Listing</button>
       </div>
     </nav>
   );
